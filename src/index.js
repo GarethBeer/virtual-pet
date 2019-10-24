704510ea79e85d35895b78b621a263432f8e8bb4
GarethBeer <gareth.beer1989@outlook.com>
2019-10-24 10:45:58 +0100

    adding prototype function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 function Pet (name) {
     this.name = name;
     this.age = 0;
-    this.growUp = function() {
-        this.age++
+    //this.growUp = function() {
+        //this.age++
     }
     
+
+
+Pet.prototype.growUp = function (){
+    this.age++
 }
-
-
 
 
 module.exports =  Pet
