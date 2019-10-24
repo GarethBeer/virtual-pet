8d58960d8ad5616f1fab7b943c62c2bc8faa07a2
GarethBeer <gareth.beer1989@outlook.com>
2019-10-24 12:02:10 +0100

    adding hunger and fitness key/values to object and adding more methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function Pet (name) {
     this.name = name;
     this.age = 0;
+    this.fitness = 10
+    this.hunger = 0
     //this.growUp = function() {
         //this.age++
     }
@@ -9,6 +11,8 @@ function Pet (name) {
 
 Pet.prototype.growUp = function (){
     this.age++
+    this.hunger = this.hunger +5
+    this.fitness = this.fitness -3
 }
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -25,3 +25,30 @@ describe('Create a method on the object', () => {
         expect(dog.age).toBe(1)
     })
 });
+
+describe('Create a prototype for the pet which changes multiple values', () => {
+    test('testing hunger has initial to 0', () => {
+        const dog = new Pet ('Max');
+        expect(dog.hunger).toBe(0);
+    })
+    test('testing fitness has initial value of 10', () => { const dog = new Pet ('Max');
+                
+        expect(dog.name).toStrictEqual('Max')
+        expect(dog.age).toBe(0);
+        expect(dog.hunger).toBe(0);
+        expect(dog.fitness).toBe(10);
+    })
+    test('testing method increases initial values when called', () => { 
+        const dog = new Pet ('Max');
+                
+        expect(dog.name).toStrictEqual('Max')
+        expect(dog.age).toBe(0);
+        expect(dog.hunger).toBe(0);
+        expect(dog.fitness).toBe(10);
+        dog.growUp();
+        expect(dog.age).toBe(1);
+        expect(dog.hunger).toBe(5);
+        expect(dog.fitness).toBe(7);
+        console.log(dog.age,dog.hunger,dog.fitness)
+    })
+});
